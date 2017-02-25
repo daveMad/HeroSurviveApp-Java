8d0f6ff75d7a54e58066ae3d0cbf57610cb13854
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-25 18:05:09 +0200

    change update() on gameLogic

--- src/main/java/com/herosurvive/app/GameLogic.java
+++ src/main/java/com/herosurvive/app/GameLogic.java
@@ -9,6 +9,14 @@ import com.herosurvive.service.*;
 public class GameLogic {
 	Boolean finished = false; // represented whether the resources are collected or not
 	ArrayList<String> output = new ArrayList<String>();
+	GameMap map = new GameMap();
+	Hero hero;
+	
+	public GameLogic(Hero hero){
+		// to do : set up game map with the parameters, the ettity classes, then invoke update()
+		this.hero = hero;
+	}
+	
 	public void start(){
 		// to do : 
 		// set the flag to running
@@ -20,6 +28,8 @@ public class GameLogic {
 			// to do : update hero position
 			// to do : check if hero intercepts with an instance of ICharacter
 			// to do : check if hero intercepts with an instance of IResource
+			map.checkInterception();
+			
 		}
 	}
 }
