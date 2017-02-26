d55d45ee0bf13cfd5de6ca48775314c164b1a329
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-26 06:48:36 +0200

    add writeSpecificFile() and some helpers on FileLogic

--- src/main/java/com/herosurvive/service/FileLogic.java
+++ src/main/java/com/herosurvive/service/FileLogic.java
@@ -10,6 +10,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -21,9 +22,12 @@ import com.herosurvive.app.App;
 public class FileLogic {
 	String fileNameWithPath;
 	String invalidMessage = "Not specified a valid file!";
+	String fileNotFoundMessage = invalidMessage + " : File Not Exists!";
+	InputLogic logic;
 
 	public FileLogic(String filePath) {
 		this.fileNameWithPath = filePath;
+		this.logic = new InputLogic();
 	}
 
 	public void read() {
@@ -38,17 +42,33 @@ public class FileLogic {
 
 	public void readFromSpecifiedFile() throws IOException {
 		String info = "Please enter the input file path (e.g : C:/file.input ):";
-		InputLogic logic = new InputLogic();
 		String inputFile = logic.getInput(info);
 		if (inputFile.trim() == "") {
 			App.Log(invalidMessage);
 			return;
 		}
 		if (!new File(inputFile).exists())
-			App.Log(invalidMessage + " : File Not Exists! => " + inputFile);
-		
+			App.Log(fileNotFoundMessage + inputFile);
+
 		this.fileNameWithPath = inputFile;
 		readSmall();
+	}
+
+	public void writeToSpecifiedFile(String file) throws IOException {
+		String info = "Please enter the output file path (e.g : C:/file.output ):";
+		String outputFile = logic.getInput(info);
+		if (outputFile.trim() == "") {
+			App.Log(invalidMessage);
+			return;
+		}
+
+		if (!new File(outputFile).exists())
+			App.Log(fileNotFoundMessage + " : " + outputFile);
+		
+		this.fileNameWithPath = outputFile;
+		Path path = Paths.get(outputFile);
+		write(path, getSampleData());
+
 	}
 
 	public void readSmall() throws IOException {
@@ -61,10 +81,36 @@ public class FileLogic {
 
 	public void writeSmall() throws IOException {
 		Path path = Paths.get(this.fileNameWithPath);
-		List<String> lines = Files.readAllLines(path);
-		lines.add("amazed");
-		Files.write(path, lines, StandardCharsets.UTF_8);
-
+		
+		write(path, getSampleData());
 	}
+	
+	
+	List<String> read(Path path) throws IOException {
+		try {
+			if (!new File(this.fileNameWithPath).exists())
+				throw new FileNotFoundException(this.fileNotFoundMessage);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Files.readAllLines(path);
+	}
+
+	void write(Path path, List<String> data) {
+		try {
+			Files.write(path, data, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	List<String> getSampleData(){
+		List<String> lines = new ArrayList<String>();
+		lines.add("amazed by writing skills!");
+		return lines;
+	}
+
 
 }
