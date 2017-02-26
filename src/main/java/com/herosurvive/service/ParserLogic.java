5356909d66eb1b495f9c9477dd7fc56ad9088e65
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-26 23:47:43 +0200

    add checkIfContainsKeyword() on parserLogic

--- src/main/java/com/herosurvive/service/ParserLogic.java
+++ src/main/java/com/herosurvive/service/ParserLogic.java
@@ -30,25 +30,25 @@ public class ParserLogic {
 		parsedData.resourcePoint = parseSpecificLine(inputData.get(0));
 		parsedData.hero.hp = parseSpecificLine(inputData.get(1));
 		parsedData.hero.attackPoint = parseSpecificLine(inputData.get(2));
-		// now get each enemy of unknown number 
-		
+		// now get each enemy of unknown number
+
 		List<Enemy> enemies;
-		
-		
+
 		// get the known number of enemies position
 	}
 	
-	class RandomData{
-		String propName;
-		int value;
+	public List<Enemy> extractEnemies(List<String> inputData){
+		int index = 3; // enemy declaration starts at Line 4 = index 3
+		while(index < inputData.size()){
+			
+		}
 		
-		public int getValue() {
-			return value;
-		}
+		return null;
+	}
 
-		public void setValue(int value) {
-			this.value = value;
-		}
+	class RandomData {
+		String propName;
+		Object value;
 
 		public String getPropName() {
 			return propName;
@@ -57,22 +57,38 @@ public class ParserLogic {
 		public void setPropName(String propName) {
 			this.propName = propName;
 		}
-		
+
 	}
-	
+
 	public RandomData parseRandomLine(String str){
+		RandomData result = new RandomData();
 		String[] array = str.split(" ");
 		for (String item : array) {
+			item = item.toLowerCase();
 			if(keywords.indexOf(item) != -1){
-				
+				String temp =keywords.get(keywords.indexOf(item));
+				if(temp == "enemy"){ // due to jdk 1.8 possible issues, used if blocks instead of switch
+					result.propName = "Enemy.name";
+					result.value = array[0]; // *Bug* is Enemy
+				}else if(temp == "has"){
+					result.propName = "Enemy.hp";
+					result.value = Integer.parseInt(array[2]); // hp value stands right before the last word of the sentence
+				} else if(temp == "attack"){
+					result.propName = "Enemy.attackPoint";
+					result.value = Integer.parseInt(array[array.length - 1]); // attack is the last word
+				} else if(temp == "position"){
+					result.propName = "Enemy.position";
+					result.value = Integer.parseInt(array[array.length - 1]); // position is the last word
+				}
 			}
 		}
 		
 		return null;
 	}
-	
-	public int parseSpecificLine(String resourceLine) { // Example : Resources are 7500
-															// meters away
+
+	public int parseSpecificLine(String resourceLine) { // Example : Resources
+														// are 7500
+														// meters away
 		String[] array = resourceLine.split(" ");
 		for (String item : array) {
 			if (checkIfNumber(item)) {
@@ -82,18 +98,23 @@ public class ParserLogic {
 
 		return 0; //
 	}
+	
+	public Boolean checkIfContainsKeyword(String line,String keyword){
+		List<String> words = Arrays.asList(line.split(" "));
+		return words.indexOf(keyword) != -1;
+	}
 
 	public String[] getKeywords() {
 		return "resources*hero*enemy*has*attack*there is a".split("\\*");
 	}
-	
-	public int extractNumber(String[] inputArray){
+
+	public int extractNumber(String[] inputArray) {
 		for (String item : inputArray) {
-			if(checkIfNumber(item) == true)
+			if (checkIfNumber(item) == true)
 				return Integer.parseInt(item);
 		}
-		
-		return 0; // return 
+
+		return 0; // return
 	}
 
 	Boolean checkIfNumber(String input) {
