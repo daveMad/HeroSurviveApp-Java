4b6ce73599ef7d47e65ee26933aa20ad9cece31f
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-26 08:07:44 +0200

    change logService structure on methods

--- src/main/java/com/herosurvive/service/LogService.java
+++ src/main/java/com/herosurvive/service/LogService.java
@@ -1,12 +1,18 @@
 package com.herosurvive.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.herosurvice.models.*;
 import com.herosurvive.app.App;
 
 public class LogService {
 	private static LogService _logger;
 
+	private List<String> fullOutput;
+
 	private LogService() {
+		fullOutput = new ArrayList<String>();
 	}
 
 	public static synchronized LogService getInstance() {
@@ -16,33 +22,37 @@ public class LogService {
 		return _logger;
 	}
 
-	public String logBattleWin(Hero hero, Enemy enemy) {
+	public void logBattleWin(Hero hero, Enemy enemy) {
 		String message = "Hero defeated " + enemy.name + " with " + hero.hp + " HP remaining";
 		App.Log(message);
-		return message;
+		add(message);
 	}
 
-	public String logBattleDefeat(Hero hero, Enemy enemy) {
+	public void add(String message) {
+		fullOutput.add(message);
+	}
+
+	public void logBattleDefeat(Hero hero, Enemy enemy) {
 		String message = enemy.name + " defeated Hero with " + enemy.hp + " HP remaining";
 		App.Log(message);
-		return message;
+		add(message);
 	}
 
-	public String logDeath(Hero hero) {
+	public void logDeath(Hero hero) {
 		String message = "Hero is Dead!! Last seen at position " + hero.position + "!!";
 		App.Log(message);
-		return message;
+		add(message);
 	}
 
-	public String logWin(Hero hero) {
+	public void logWin(Hero hero) {
 		String message = "Hero Survived!";
 		App.Log(message);
-		return message;
+		add(message);
 	}
 
-	public String logStart(Hero hero) {
+	public void logStart(Hero hero) {
 		String message = "Hero started journey with " + hero.hp + "HP!";
 		App.Log(message);
-		return message;	
+		add(message);
 	}
 }
